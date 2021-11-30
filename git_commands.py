6d8d4ef13c12319c6fbfad132774bd3d83750433
beibars <mukashov.beibars2003@gmail.com>
2021-11-30 12:46:41 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,16 @@
+git init - .git 
+git remote add name url  вместо nameurl добавляем https,ssh ссылку добавляет 
+удаленный репозиторий
+git pull origin master - стягивает все изменения с ветки master
+git status - показывает статус файла проектов
+git add - добавлет файлы в рабочей папке в индекс т.е. в stagin area
+git add name file - то добавит файл с именем
+git commit - добавляет все файлы которые находятся в индексе и сохраняет их 
+во внутренню базу
+git branch - менеджер веток.можно узнать список веток и
+git branch name branch - создает ветку
+git checkout name branch  - преключение на ветку
+git push origin master - отправка кода в удаленным репозиторий
+git reset filename - отвесает за удаление файла из stagin area
+
+
